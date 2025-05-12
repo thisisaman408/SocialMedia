e95ab655415b1f11963b2b9d7f695e54951fa007
Aman Kumar <thisisaman408@gmail.com>
2025-05-13 03:55:46 +0530

    Working

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,25 +1,25 @@
-import { Outlet, Navigate } from "react-router-dom";
+import { Navigate, Outlet } from 'react-router-dom';
 const AuthLayout = () => {
-  const isAuthenticated = false;
-  return (
-    <>
-      {isAuthenticated ? (
-        <Navigate to="/" />
-      ) : (
-        <>
-          <section className="flex flex-1 justify-center items-center flex-col py-10">
-            <Outlet></Outlet>
-          </section>
+	const isAuthenticated = false;
+	return (
+		<>
+			{isAuthenticated ? (
+				<Navigate to="/" />
+			) : (
+				<>
+					<section className="flex flex-1 justify-center items-center flex-col py-10">
+						<Outlet></Outlet>
+					</section>
 
-          <img
-            src="assets/images/side-img.svg"
-            alt="SideImage"
-            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
-          />
-        </>
-      )}
-    </>
-  );
+					<img
+						src="assets/images/side-img.png"
+						alt="SideImage"
+						className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+					/>
+				</>
+			)}
+		</>
+	);
 };
 
 export default AuthLayout;
